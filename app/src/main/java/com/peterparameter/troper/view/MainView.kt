3538ae6ebae7f725ae5b408e73edf433fb76ce66
Unknown <peter.nosko@gmail.com>
2019-06-03 01:31:06 +0200

    Improve layout of start screen

--- app/src/main/java/com/peterparameter/troper/view/MainView.kt
+++ app/src/main/java/com/peterparameter/troper/view/MainView.kt
@@ -1,7 +1,9 @@
 package com.peterparameter.troper.view
 
 import android.content.Context
+import android.widget.LinearLayout
 import com.peterparameter.troper.R
+import splitties.dimensions.dip
 import splitties.views.dsl.coordinatorlayout.coordinatorLayout
 import splitties.views.dsl.coordinatorlayout.defaultLParams
 import splitties.views.dsl.core.Ui
@@ -17,43 +19,27 @@ class MainView(override val ctx: Context) : Ui {
     override val root by lazy {
         coordinatorLayout {
             fitsSystemWindows = true
-            add(
-                verticalLayout {
-                    add(
-                        m.button.text { textResource = R.string.random_caption },
-                        lParams { gravity = gravityCenterHorizontal })
-//                add(favorites, lParams { gravity = gravityCenterHorizontal } )
-                },
-                defaultLParams { })
+
+            add(content, defaultLParams { })
         }
     }
 
-//    val content = verticalLayout {
-//        add(favorites, lParams {
-//            gravity = gravityCenterHorizontal
-//            topMargin = dip(8)
-//        }).setupWith{ println(it)}
-//        add(random, lParams {
-//            gravity = gravityCenterHorizontal
-//            topMargin = dip(8)
-//        }).setupWith{ println(it)}
-//        add(offline, lParams {
-//            gravity = gravityCenterHorizontal
-//            topMargin = dip(8)
-//        })
-//        add(recent, lParams {
-//            gravity = gravityCenterHorizontal
-//            topMargin = dip(8)
-//        })
-//    }
+    val content by lazy {
+        verticalLayout {
+            add(favorites, placement())
+            add(random, placement())
+            add(offline, placement())
+            add(recent, placement())
+        }
+    }
 
-//    val favorites = m.button.text { textResource = R.string.favorites_caption }
+    val favorites = m.button.text { textResource = R.string.favorites_caption }
     val random = m.button.text { textResource = R.string.random_caption }
-//    val offline = button { textResource =  R.string.offline_caption }
-//    val recent = button { textResource =  R.string.recent_caption }
-//
-//    private fun LinearLayout.placement() = lParams {
-//        gravity = gravityCenterHorizontal
-//        topMargin = dip(8)
-//    }
+    val offline = m.button.text { textResource =  R.string.offline_caption }
+    val recent =  m.button.text { textResource =  R.string.recent_caption }
+
+    private fun LinearLayout.placement() = lParams {
+        gravity = gravityCenterHorizontal
+        topMargin = dip(8)
+    }
 }
